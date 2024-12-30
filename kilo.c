7d675a2d67a0a8db0862604defe3e8ce45015668
Chijioke Ugwuanyi <cj.ugwuanyi@gmail.com>
2024-12-30 08:49:16 +0200

    Can now take in user input.

--- kilo.c
+++ kilo.c
@@ -1,7 +1,30 @@
 
 #include <stdio.h>
+#include <unistd.h>
+#include <readline/readline.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
 int main() {
-    printf("Hello, world! \n");
+    char *input;
+    bool time_to_exit = false;
+
+    while (!time_to_exit) {
+        input = readline("");
+        if (input == NULL) {
+            time_to_exit = true;
+            goto loop_end;
+        }
+
+        if (*input == '\0')   // user just hit enter, no content
+            goto loop_end;
+
+        loop_end:
+
+            free(input);
+            input = NULL;
+
+    }
+
     return 0;
 }
